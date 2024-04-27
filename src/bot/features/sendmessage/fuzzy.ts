dd014c7430318decaeb6b9837a6e4ef1dae1ae84
chuangcaleb <balechuang@gmail.com>
2024-04-27 23:04:22 +0800

    feat: collect array of names

--- src/bot/features/sendmessage/fuzzy.ts
+++ src/bot/features/sendmessage/fuzzy.ts
@@ -1,12 +1,14 @@
 import Fuse from "fuse.js";
 import { REGISTRY_ARRAY } from "./mock-data.js";
 
-const THRESHOLD = 0.4;
+const THRESHOLD = 0.3;
 const LIMIT = 3;
 
 const fuse = new Fuse(REGISTRY_ARRAY, {
   threshold: THRESHOLD,
   keys: [{ name: "name", getFn: (pair) => pair[0] }],
+  includeScore: true,
+  ignoreLocation: true,
 });
 
 export function retrieveQueryResult(query: string) {
--- src/bot/features/sendmessage/sendmessage.conversation.ts
+++ src/bot/features/sendmessage/sendmessage.conversation.ts
@@ -11,41 +11,49 @@ export function sendmessageConversation() {
       await conversation.run(i18n);
 
       // Get message
-      await ctx.reply("Please tell me the message you want to send");
+      // await ctx.reply("Please tell me the message you want to send");
 
-      const messageCtx = await conversation.waitFor("message:text", {
-        otherwise: async (ctxx) => {
-          await ctxx.reply("please send bold");
-          await conversation.skip({ drop: true });
-        },
-      });
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const message = messageCtx.message?.text ?? "";
+      // const messageCtx = await conversation.waitFor("message:text", {
+      //   otherwise: async (ctxx) => {
+      //     await ctxx.reply("please send bold");
+      //     await conversation.skip({ drop: true });
+      //   },
+      // });
+      // // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      // const message = messageCtx.message?.text ?? "";
 
       // Get targets
-      await ctx.reply("Please type the name of a student");
-      const nameCtx = await conversation.waitFor("message:text", {
-        otherwise: async (ctxx) => {
-          await ctxx.reply("please send name");
-          await conversation.skip({ drop: true });
-        },
-      });
-      const name = nameCtx.message?.text ?? "";
-      const queryResults = retrieveQueryResult(name);
-      // if no results
-      if (queryResults.length === 0) {
-        ctx.reply("no results, please retry");
-        return;
+      // eslint-disable-next-line no-constant-condition
+      const students = [];
+      while (true) {
+        await ctx.reply("Please type the name of a student, or send /done");
+        const nameCtx = await conversation.waitFor("message:text", {
+          otherwise: async (ctxx) => {
+            await ctxx.reply("please send name");
+            await conversation.skip({ drop: true });
+          },
+        });
+        const name = nameCtx.message?.text ?? "";
+        if (name === "/done") break;
+        const queryResults = retrieveQueryResult(name);
+        // if no results
+        if (queryResults.length === 0) {
+          ctx.reply("no results, please retry");
+          continue;
+        }
+        // if more than 1
+        // TODO: handle multiple with further narrowing
+        if (queryResults.length > 1) {
+          const names = queryResults.map((r) => r.item[0] + r.score).join(", ");
+          await ctx.reply(`many results (${names}), please retry`);
+          continue;
+        }
+        // if exactly 1
+        const result = queryResults[0].item;
+        students.push(result[1]);
       }
-      // if more than 1
-      // TODO: handle multiple with further narrowing
-      if (queryResults.length > 1) {
-        const names = queryResults.map((r) => r.item[0]).join(", ");
-        ctx.reply(`many results (${names}), please retry`);
-        return;
-      }
-      // if exactly 1
-      await ctx.reply(`Sending to ${queryResults[0].item[0]}: ${message}`);
+      await ctx.reply(`Sending to ${students.join(",")}: `);
+      // await ctx.reply(`Sending to ${result}: ${message}`);
     },
     SENDMESSAGE_CONVERSATION
   );
