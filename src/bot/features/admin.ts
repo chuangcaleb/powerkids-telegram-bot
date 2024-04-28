1478109b7eef29e4d101fc728b5cc32ccfbb6148
chuangcaleb <balechuang@gmail.com>
2024-04-28 16:56:12 +0800

    refac: move filters into helpers

--- src/bot/features/admin.ts
+++ src/bot/features/admin.ts
@@ -1,8 +1,8 @@
 import { chatAction } from "@grammyjs/auto-chat-action";
 import { Composer } from "grammy";
 import type { Context } from "~/bot/context.js";
-import { isAdmin } from "~/bot/filters/index.js";
 import { setCommandsHandler } from "~/bot/handlers/index.js";
+import { isAdmin } from "~/bot/helpers/filters/index.js";
 import { logHandle } from "~/bot/helpers/logging.js";
 
 const composer = new Composer<Context>();
