fdb07d928c082e7bea63634275ef4779c46fd36f
chuangcaleb <balechuang@gmail.com>
2024-05-11 16:52:25 +0800

    clean: remove Admin type

--- src/lib/directus/methods/get-admins.ts
+++
@@ -1,8 +0,0 @@
-import { readUsers } from "@directus/sdk";
-import { client } from "../client.js";
-
-export function getAdmins() {
-  return client.request(
-    readUsers({ fields: ["id", "first_name", "telegram_ids"], limit: 999 })
-  );
-}
--- src/lib/directus/types-gen.ts
+++ src/lib/directus/types-gen.ts
@@ -1,10 +1,7 @@
 import { Identity } from "@directus/sdk";
 import { getStudents } from "./methods/get-students.js";
-import { getAdmins } from "./methods/get-admins.js";
 import { getParents } from "./methods/get-parents.js";
 
 export type Student = Identity<Awaited<ReturnType<typeof getStudents>>[0]>;
-
-export type Admin = Identity<Awaited<ReturnType<typeof getAdmins>>[0]>;
 
 export type Parent = Identity<Awaited<ReturnType<typeof getParents>>[0]>;
